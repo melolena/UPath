1acdd90d00fb8d41676fa0b08b63c8cf3f8720b5
Milena Santos de Melo <1241302151@acd.grupotiradentes.com>
2025-10-08 20:34:17 -0300

    adicionando volta ao login

--- app/src/main/java/com/example/upath/Register.java
+++ app/src/main/java/com/example/upath/Register.java
@@ -1,6 +1,8 @@
 package com.example.upath;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,19 @@ public class Register extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+
+    public void goToMainActivity(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
     }
 }
